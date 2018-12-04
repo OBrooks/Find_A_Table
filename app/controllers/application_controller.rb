08416ba565c94d4c7aa5b17db618389b3f8911b1
paulo-guerin <paularguerin@gmail.com>
2018-12-04 12:52:44 +0100

    Première implémentation devise et ajout de colones pour la table User, égalment modals d'inscription et de connection et display page webmaster et admin avec un statut

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :sanitize_devise_params, if: :devise_controller?
+    
+    def sanitize_devise_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :town, :adress, :status, :gender, :profile_picture, :experience, :description])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :town, :adress, :status, :gender, :profile_picture, :experience, :description])
+    end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "home#lp"
+  get "index", to: "home#index"
+  get "profile", to: "home#profile"
+  get "webmaster", to: "home#webmaster"
+  get "admin", to: "home#admin"
 end
