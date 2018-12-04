53928ace03705d8102211cb42b866624d3431b3e
paulo-guerin <paularguerin@gmail.com>
2018-12-04 21:18:01 +0100

    Début de scrapper

--- app/services/gamescrap.rb
+++ app/services/gamescrap.rb
@@ -7,33 +7,53 @@ class Gamescrap
     def initialize
         @page_scrap = 1
         @url = "https://www.trictrac.net/jeu-de-societe/liste/les-tops?page=#{@pagescrap}"
+        @games_infos = []
     end
 
     def perform
-        save(@url)
+        scrap_games_links(@url)
     end
 
-    def save(url)
-        games_urls = []
-        page = Nokogiri::HTML(open(url))  
-        #html.whatinput-types-initial.whatinput-types-mouse body div#content div.row div#content-column.small-12.medium-8.large-9.columns div.items.divided.data div.item div.content a.header
-        path_game = page.css("a.header")
-        path_game.each do |game|
-            game_url = game.text
-            games_urls << game_url
-            puts game_url
-        end
-
-        # crypto_value = []
-        # path_value= page.css("a.price")
-        # path_value.each do |section|
-        #     crypto_value << section.text
-        # end
+    def scrap_game_infos(url_game)
+        temporary_array = []
         
-        # compteur = 0
-        # while compteur <= crypto_name.length
-        #     Crypto.create!(crypto_name: crypto_name[compteur], crypto_value: crypto_value[compteur])
-        #     compteur = compteur + 1
+        page_game = Nokogiri::HTML(open(url_game))
+        puts url_game
+        # page_game.xpath('/html/body/div[4]/header/div/div[2]/div[1]/div/h1/a').each do |section|
+        #     temporary_array << section.text
         # end
+
+        page_game.xpath('/html/body/div[4]/div/div[2]/div[1]/div[2]').each do |section|
+            temporary_array << section.content
+        end
+        puts temporary_array
+
+        page_game.xpath('/html/body/div[4]/div/div[2]/div[1]/div[2]').each do |section|
+            temporary_array << section.content
+        end
+        puts temporary_array
+
+        
+
+        
     end
+    
+    def scrap_games_links(url)
+        games_urls = []
+        main_page = Nokogiri::HTML(open(url))
+        #html.whatinput-types-initial.whatinput-types-mouse body div#content div.row div#content-column.small-12.medium-8.large-9.columns div.items.divided.data div.item div.content a.header
+        path_game = main_page.css("a.header")
+        array_tempo = []
+        path_game.each do |game| 
+            game.each do |components|
+                if components[0] == "href"
+                    if components[1][0..5] == "https:"
+                        scrap_game_infos(components[1])
+                    end
+                end
+            end
+        end  
+    end
+
+    
 end
