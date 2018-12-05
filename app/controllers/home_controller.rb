475f163019e5c40f4664d4225a1213a954ffdd52
OBrooks <mr.willh@gmail.com>
2018-12-05 19:06:00 +0100

    correct Home Controller Webmaster

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,16 +7,16 @@ class HomeController < ApplicationController
     end
 
     def webmaster
-        if current_user.status == 3
+        if current_user.webmaster
         else 
-            redirect_to "home/index"
+            redirect_to root
         end
     end
 
     def admin
-        if current_user.status == 2
+        if current_user.admin
         else 
-            redirect_to "home/index"
+            redirect_to root
         end
     end
 
