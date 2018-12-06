7093305c4fc6e99eeb55a3a386e0a7663a1c73d8
paulo-guerin <paularguerin@gmail.com>
2018-12-06 18:03:48 +0100

    CRUD games basique

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,15 @@ class GamesController < ApplicationController
         end
     end
 
+    def new
+        @game = Game.new
+    end
+
+    def create
+        Game.create!(title: params[:game][:title], description: params[:game][:description], image_url: params[:game][:image_url], min_players: params[:game][:min_players], max_players: params[:game][:max_players], time: "#{params[:game][:time]}" + " min", category: params[:game][:category])
+        redirect_to games_path
+    end
+
     def show
         @game=Game.find(params[:id])
     end
@@ -14,4 +23,9 @@ class GamesController < ApplicationController
         @game=Game.find(params[:id])
     end
 
+    def update
+        @game=Game.find(params[:id])
+        @game.update(title: params[:game][:title], description: params[:game][:description], image_url: params[:game][:image_url], min_players: params[:game][:min_players], max_players: params[:game][:max_players], time: "#{params[:game][:time]}" + " min", category: params[:game][:category])
+        redirect_to games_path
+    end
 end
