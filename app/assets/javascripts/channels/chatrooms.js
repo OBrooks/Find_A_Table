5a091c9a5173a07fe706e1f38011201dca903d01
OBrooks <mr.willh@gmail.com>
2018-12-12 17:01:26 +0100

    Uglifier doesn't like let -> var

--- app/assets/javascripts/channels/chatrooms.js
+++ app/assets/javascripts/channels/chatrooms.js
@@ -6,7 +6,7 @@ App.chatrooms = App.cable.subscriptions.create ("ChatroomsChannel",{
     console.log('voici chatroom_id', data['chatroom_id'])
     console.log('voici data-chatroom-id',"[data-chatroom-id='" + data['chatroom_id'] + "']")
 
-    let active_chatroom = $("#usersmessages").find("[data-chatroom-id='" + data['chatroom_id'] + "']");
+    var active_chatroom = $("#usersmessages").find("[data-chatroom-id='" + data['chatroom_id'] + "']");
 
     console.log(active_chatroom)
     console.log(data['usersmessage'])
